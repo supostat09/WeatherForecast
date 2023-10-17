da40a072f569cadb7874be856955031bf38d6a99
dzhumadil <dzhumadil.a@icloud.com>
2023-10-17 19:47:42 +0600

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Абдулла-Бек on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
